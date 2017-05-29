34c9d741c480f91f67aba44296daa617a6d801bc
EKKAPONG <gold_exp@hotmail.com>
2017-05-29 14:17:45 +0700

    Add project files.

---
+++ dotnet-dev-calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_dev_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
